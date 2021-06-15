96dbaafc9ef34ba14d21e5d15c5c98b5d510d701
Julien <peloton@lal.in2p3.fr>
2021-06-15 14:58:49 +0200

    remove tty

--- build_image.sh
+++ build_image.sh
@@ -25,12 +25,12 @@ docker build -t "spark_escape2021" -f Dockerfile .
 SPARKFITS="com.github.astrolabsoftware:spark-fits_2.12:0.9.0"
 
 # Check installation worked
-docker run -it --rm  \
+docker run --rm  \
   -v $PWD:/home/jovyan/work:rw -p 8888:8888 -p 4040:4040 -p 18080:18080 \
   spark_escape2021 spark-submit --master local[*] \
   --driver-memory 2g --executor-memory 2g --packages $SPARKFITS test_installation.py
 
 # Run unit tests
-docker run -it --rm  \
+docker run --rm  \
   -v $PWD:/home/jovyan/work:rw -p 8888:8888 -p 4040:4040 -p 18080:18080 \
   spark_escape2021 ./test.sh
